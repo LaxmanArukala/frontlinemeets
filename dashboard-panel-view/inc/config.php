5873d52316d0cd59624d6830ee295c7d67b501e9
Harish-komatireddys <harishkomatireddys@gmail.com>
2023-04-09 11:43:38 +0530

    Update DB Change

--- dashboard-panel-view/inc/config.php
+++ dashboard-panel-view/inc/config.php
@@ -1,6 +1,6 @@
 <?php
-//  $con = mysqli_connect("localhost","u216678667_AGM_Us","*Ce6#vQ$%/8i_AG","u216678667_AGM_Ds");
- $con = mysqli_connect("localhost","root","","agmeets");
+$con = mysqli_connect("localhost","u216678667_AGM_Us","*Ce6#vQ$%/8i_AG","u216678667_AGM_Ds");
+//$con = mysqli_connect("localhost","root","","agmeets");
 if (!$con) {
     die("Connection failed: " . mysqli_connect_error($con));
 }
